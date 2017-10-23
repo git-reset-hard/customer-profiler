f2d3da1cb6d9b93efb8d5a108dfdf9310b800190
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-23 11:35:31 -0700

    (feat) Create DB tables

--- database/index.js
+++ database/index.js
@@ -18,4 +18,121 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+const User = sequelize.define('user', {
+  // need name to populate reviews if passing unique users to RP
+  name: {
+    type: Sequelize.TEXT
+  },
+  gets_recommendations: {
+    type: Sequelize.BOOLEAN
+  },
+  star_importance: {
+    type: Sequelize.FLOAT
+  },
+  proximity_importance: {
+    type: Sequelize.FLOAT
+  },
+  price_importance: {
+    type: Sequelize.FLOAT
+  },
+  restaurant_variance: {
+    type: Sequelize.FLOAT
+  },
+  home_city: {
+    type: Sequelize.STRING
+  },
+  home_coordinates: {
+    type: Sequelize.JSONB // lat, long
+  },
+  personality: {
+    type: Sequelize.JSONB // watson object
+  }
+});
+
+const Restaurant = sequelize.define('restaurant', {
+  profile: {
+    type: Sequelize.JSONB
+  }
+});
+
+const Query = sequelize.define('query', {
+  search_term: {
+    type: Sequelize.TEXT
+  },
+  location: {
+    type: Sequelize.TEXT
+  }
+});
+
+const Check_In = sequelize.define('check_in', {
+  distance: {
+    type: Sequelize.FLOAT
+  },
+  time: {
+    type: Sequelize.DATE
+  }
+});
+
+Check_In.belongsTo(User);
+Check_In.belongsTo(Restaurant);
+
+
+const Review = sequelize.define('review', {
+  star_rating: {
+    type: Sequelize.INTEGER
+  },
+  time: {
+    type: Sequelize.DATE
+  },
+  body: {
+    type: Sequelize.TEXT
+  }
+});
+
+Review.belongsTo(User);
+Review.belongsTo(Restaurant);
+
+const Click = sequelize.define('click', {
+  list_id: {
+    type: Sequelize.INTEGER
+  },
+  distance: {
+    type: Sequelize.FLOAT
+  },
+  time: {
+    type: Sequelize.DATE
+  }
+});
+
+Click.belongsTo(User);
+Click.belongsTo(Restaurant);
+Click.belongsTo(Query);
+
+// TODO: Switch to force: false when DB is populated
+// force: true will drop the table if it already exists
+User.sync({force: true})
+  .then(() => {
+    return Restaurant.sync({force: true});
+  })
+  .then(() => {
+    return Query.sync({force: true});
+  })
+  .then(() => {
+    return Check_In.sync({force: true});
+  })
+  .then(() => {
+    return Review.sync({force: true});
+  })
+  .then(() => {
+    return Click.sync({force: true});
+  });
+
+// .then(() => {
+// //   // Table created
+//   return User.create({
+//     gets_recommendations: 0,
+//     star_importance: 0.5
+//   });
+// })
+
 module.exports = sequelize;
