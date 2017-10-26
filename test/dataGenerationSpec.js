690748acf54cf059466a160f0c58e14d11f1f8e8
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-25 22:02:33 -0700

    (doc) Add test file and testing to-dos for data gen

---
+++ test/dataGenerationSpec.js
@@ -0,0 +1,6 @@
+// TODO: test data generation
+// get number of rows in db, add batch of users, compare new number of rows in db
+
+// TODO: test helpers
+// makeRandomDateTime: test that date is within range
+// insert review/checkin/click and retrieve
