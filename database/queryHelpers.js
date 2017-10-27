00e56ae7459719a7f87902a46c30f5664897b755
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-26 21:58:22 -0700

    (feat) Add getUserPreferences DB query

--- database/queryHelpers.js
+++ database/queryHelpers.js
@@ -1,24 +1,53 @@
 const db = require('./index');
+const gen = require('../dataGeneration/generateData.js');
 
 const getUserPreferences = function(userId) {
-  db.User.findOne({
+  let prefs = {};
+
+  return db.User.findOne({
     where: {
       id: userId
     }
   })
     .then((result) => {
-      // return pref fields here
+      prefs = {
+        star_importance: result.dataValues.star_importance,
+        proximity_importance: result.dataValues.proximity_importance,
+        price_importance: result.dataValues.price_importance,
+        restaurant_variance: result.dataValues.restaurant_variance
+      };
+
+      console.log(prefs);
     });
-
-
 };
+
+// getUserPreferences sample output:
+// { star_importance: null,
+//   proximity_importance: null,
+//   price_importance: null,
+//   restaurant_variance: null }
+
+// db.sequelize.authenticate()
+//   .then(() => {
+//     console.log('creating user');
+//     db.User.bulkCreate(gen.makeUserBatch(2));
+//   })
+//   .then(() => {
+//     getUserPreferences(1);
+//   });
+
 
 // get user
 // pass in obj with trait-value pairs
 // update row with values
-const updateUserPreferences = function(userId, newPrefs) {
-
-};
+// const updateUserPreferences = function(userId, newPrefs) {
+//   db.User.update({
+//     where: {
+//       id: userId
+//     }
+//   })
+//   .then
+// };
 
 
 
@@ -26,11 +55,14 @@ const updateUserPreferences = function(userId, newPrefs) {
 // will be sent to rec. engine
 
 // returns promise
+// find all reviews by user
+// filter by star ratings of 4 or 5
 const getLikedRestaurants = function(userId) {
-  return db.Restaurants.findAll({
+  return db.Reviews.findAll({
     attributes: ['id'],
     where: {
-      userId: userId
+      userId: userId,
+      // [Op.gte]
     }
   });
 };
@@ -56,6 +88,6 @@ const getUserInfo = function(userId) {
   // format and return all info
 };
 
-const formatUserInfo = function(userRow, likedRestaurants) {
+// const formatUserInfo = function(userRow, likedRestaurants) {
 
-}
+// }
