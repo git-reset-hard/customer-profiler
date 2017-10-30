add851f32a8bc65f09a5a1e6afb8236647a346cd
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-29 19:56:54 -0700

    (feat) Add endpoint skeletons for adding user input to DB

--- server/index.js
+++ server/index.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const db = require('../database/index.js');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
-
-const helpers = require('../helpers/helpers.js');
 
 app.listen(port, () => {
   console.log('Server listening on port ', port);
 });
 
-  
-
 // TODO: create endpoints for POSTing clicks, reviews, check-ins
+
+app.use(bodyParser.json());
+
+app.post('/clicks', function (req, res) {
+  db.addClick(req.body)
+    .then(() => res.status(200).send('Click POSTed'))
+    .catch((err) => console.log('Error on click POST'));
+});
+
+app.post('/checkins', function (req, res) {
+  res.status(200).send('Check-in POSTed');
+});
+
+app.post('/reviews', function (req, res) {
+  res.status(200).send('Review POSTed');
+});
+
+console.log(db);
