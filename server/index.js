deb4d01ef51fc80ae6b2ae911ec131abf7a4cc88
Christie Villanueva <christiejvillanueva@gmail.com>
2017-10-31 14:11:46 -0700

    (feat) Implement sending log files to elasticsearch, switch to aws

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 let nextRestaurantId, nextUserId;
 
 const express = require('express');
+const query = require('../database/queryHelpers.js');
 const db = require('../database/index.js');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const Promise = require('bluebird');
+const appendFile = Promise.promisify(fs.appendFile);
 const port = 8080;
+
 
 app.listen(port, () => {
   console.log('Server listening on port ', port);
@@ -21,6 +26,7 @@ query.getCurrentRestaurantId()
 app.use(bodyParser.json());
 
 app.post('/clicks', function (req, res) {
+
   query.addClick(click)
     .then(() => res.status(200).send('Click POSTed'))
     .catch((err) => console.log('Error on click POST'));
@@ -33,6 +39,7 @@ app.post('/checkins', function (req, res) {
 });
 
 app.post('/reviews', function (req, res) {
+
   query.addReview()
     .then(() => res.status(200).send('Review POSTed'))
     .catch((err) => console.log('Error on review POST'));
