9cb55ddb2657612594276a398fc7157915bb566d
Christie Villanueva <christiejvillanueva@gmail.com>
2017-11-04 11:48:52 -0700

    (feat) Pseudocode processing queries from app server
    Hold off on completing until query format is finalized

--- sqs/queriesFromAnalytics.js
+++ sqs/queriesFromAnalytics.js
@@ -2,6 +2,7 @@ const config = require('../config/config.js');
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config/config.json');
 const Consumer = require('sqs-consumer');
+const helpers = require('../dataGeneration/seedHelpers.js');
 
 const app = Consumer.create({
   queueUrl: config.fromAnalytics,
@@ -20,3 +21,13 @@ app.on('error', (err) => {
 });
  
 app.start();
+
+// INCOMPLETE: finish after getting query format
+const makeClicksFromQuery = function(query) {
+  let clicks = [];
+  let restaurants = [];
+  // TODO: get list of restaurants
+  // randomize number of clicks = n
+  // randomize n restaurants
+
+};
